5914f3f6469203ccbd80c3169d30e5cd5b14a079
BenFischer001 <benfischer52@gmail.com>
2024-01-03 21:57:12 -0600

    add info card component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Nav from './Components/Nav';
 import Title from './Components/Title';
 import Line from './Components/Line';
+import InfoCard from './Components/InfoCard';
 
 import './App.css';
 
@@ -11,6 +12,9 @@ function App() {
         <Title title={'Good Morning'} underText={'tag line'}/>
         <Line/>
         <Title title={'About Me'}/>
+        <InfoCard title={'Tagging Specialist'}/>
+        <InfoCard title={'Front End Developer'}/>
+        <InfoCard title={'Data Analyst'}/>
     </div>
   );
 }
---
+++ src/Components/InfoCard.js
@@ -0,0 +1,12 @@
+import './CSS/InfoCard.css'
+
+const InfoCard = ({title}) => {
+    return (        
+        <div className='infoCard'>
+            <h2>{title}</h2>
+            
+        </div>
+    )
+}
+
+export default InfoCard
